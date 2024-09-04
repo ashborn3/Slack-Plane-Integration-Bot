787e2fbb8a8c48ffe1f22ded18814ec5c315a445
ashborn3 <114130969+ashborn3@users.noreply.github.com>
2024-09-04 20:45:54 +0530

    Added struct and funtion to fetch state of a task Added Socketmode for events Added function to load user_mapping Added function to send issue details to assignees Added function to categorize issues and send daily overview

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.22.5
 
 require github.com/slack-go/slack v0.14.0
 
+require github.com/robfig/cron/v3 v3.0.1 // indirect
+
 require (
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
+	"context"
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
+	"github.com/robfig/cron/v3"
 	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
+	"github.com/slack-go/slack/socketmode"
 )
 
-var slackClient *slack.Client
-var slug string
-var planeToken string
+var (
+	slug        string
+	planeToken  string
+	slackClient *slack.Client
+)
 
 type ProjectsResponse struct {
 	Count   int       `json:"count"`
@@ -72,6 +80,41 @@ type Issue struct {
 	Labels            []string    `json:"labels"`
 }
 
+type StateResponse struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func fetchStateName(projectID, stateID string) (string, error) {
+	url := fmt.Sprintf("https://api.plane.so/api/v1/workspaces/%s/projects/%s/states/%s/", os.Getenv("SLUG"), projectID, stateID)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", fmt.Errorf("error creating request: %v", err)
+	}
+
+	req.Header.Add("x-api-key", os.Getenv("PLANE_TOKEN"))
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("error sending request: %v", err)
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", fmt.Errorf("error reading response body: %v", err)
+	}
+
+	var stateResponse StateResponse
+	err = json.Unmarshal(body, &stateResponse)
+	if err != nil {
+		return "", fmt.Errorf("error unmarshaling response body: %v", err)
+	}
+
+	return stateResponse.Name, nil
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -81,10 +124,82 @@ func main() {
 	slug = os.Getenv("SLUG")
 	planeToken = os.Getenv("PLANE_TOKEN")
 
-	slackClient = slack.New(os.Getenv("SLACK_TOKEN"))
+	slackClient = slack.New(os.Getenv("SLACK_TOKEN"), slack.OptionDebug(true), slack.OptionAppLevelToken(os.Getenv("SLACK_SOCK_TOKEN")))
 
-	fmt.Println(fetchIssues(fetchProjects()))
+	socketClient := socketmode.New(slackClient, socketmode.OptionDebug(true))
 
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go startSocketMode(ctx, slackClient, socketClient)
+
+	sendDailyOverview()
+	fmt.Print("sendDailyOverview() executed\n")
+	notifyUsersDaily()
+	fmt.Print("notifyUsersDaily() executed\n")
+
+	// Set up the cron job
+	c := cron.New(cron.WithLocation(time.FixedZone("IST", 5*60*60+30*60))) // IST is UTC+5:30
+	_, err = c.AddFunc("0 9 * * *", notifyUsersDaily)                      // Run at 9:00 AM IST every day
+	if err != nil {
+		log.Fatalf("Error setting up cron job: %v", err)
+		return
+	}
+	c.Start()
+
+	// Run the Socketmode client
+	socketClient.Run()
+}
+
+func handleEvent(event slackevents.EventsAPIEvent) {
+	// Add your event handling logic here
+	log.Printf("Received event: %v", event)
+}
+
+func startSocketMode(ctx context.Context, slackClient *slack.Client, sockClient *socketmode.Client) {
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("Shutting down socketmode listener")
+			return
+		case event := <-sockClient.Events:
+			switch event.Type {
+			case socketmode.EventTypeEventsAPI:
+				eventsAPI, ok := event.Data.(slackevents.EventsAPIEvent)
+				if !ok {
+					log.Printf("Could not type cast the event to the EventsAPI: %v\n", event)
+					continue
+				}
+				sockClient.Ack(*event.Request)
+				log.Println(eventsAPI)
+				// Handle the event here
+				handleEvent(eventsAPI)
+			}
+		}
+	}
+}
+
+func loadUserMapping(filePath string) (map[string]string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	userMapping := make(map[string]string)
+	for _, record := range records[1:] { // Skip header row
+		planeUUID := record[0]
+		slackUserID := record[1]
+		userMapping[planeUUID] = slackUserID
+	}
+
+	return userMapping, nil
 }
 
 func fetchProjects() []string {
@@ -168,4 +283,129 @@ func fetchIssues(projectIds []string) []IssuesResponse {
 	}
 
 	return issues
+}
+
+func sendIssueDetailsToAssignees(issues []IssuesResponse) int {
+	var message string
+	userMapping, _ := loadUserMapping("user_mapping.csv")
+
+	for _, issue := range issues {
+		for _, issueData := range issue.Results {
+			message = fmt.Sprintf("Issue: %s\nPriority: %s\nTarget Date: %s\nDescription: %s\n", issueData.Name, issueData.Priority, issueData.TargetDate, issueData.DescriptionHTML[3:len(issueData.DescriptionHTML)-4])
+			for _, assignee := range issueData.Assignees {
+				slackUserID, exists := userMapping[assignee]
+				if !exists {
+					log.Printf("No Slack user ID found for Plane UUID: %s", assignee)
+					continue
+				}
+				fmt.Print("Sending message to: ", slackUserID, "\n")
+				channel, _, _, err := slackClient.OpenConversation(&slack.OpenConversationParameters{
+					Users: []string{"U07K0L9DNUX"},
+				})
+				slackClient.PostMessage(channel.ID, slack.MsgOptionText(message, false))
+				if err != nil {
+					log.Fatalf("Error sending message: %v", err)
+					return -1
+				}
+			}
+		}
+	}
+
+	return 0
+}
+
+func notifyUsersDaily() {
+	// Fetch projects
+	projectIds := fetchProjects()
+	if projectIds == nil {
+		log.Println("No projects found")
+		return
+	}
+
+	// Fetch issues
+	issues := fetchIssues(projectIds)
+	if issues == nil {
+		log.Println("No issues found")
+		return
+	}
+
+	// Send notifications
+	sendIssueDetailsToAssignees(issues)
+}
+
+func categorizeIssues(issues []IssuesResponse) (openIssues, inProgressIssues, closedIssues, doneIssues, todoIssues []string) {
+	for _, issue := range issues {
+		for _, issueData := range issue.Results {
+			stateName, err := fetchStateName(issueData.Project, issueData.State)
+			fmt.Print("State Name: ", stateName, "\n")
+			if err != nil {
+				log.Printf("Error fetching state name: %v", err)
+				continue
+			}
+			switch stateName {
+			case "Backlog":
+				openIssues = append(openIssues, issueData.Name)
+			case "Todo":
+				todoIssues = append(todoIssues, issueData.Name)
+			case "In Progress":
+				inProgressIssues = append(inProgressIssues, issueData.Name)
+			case "Cancelled":
+				closedIssues = append(closedIssues, issueData.Name)
+			case "Done":
+				doneIssues = append(doneIssues, issueData.Name)
+			}
+		}
+	}
+	return
+}
+
+func sendDailyOverview() {
+	// Fetch projects
+	projectIds := fetchProjects()
+	if projectIds == nil {
+		log.Println("No projects found")
+		return
+	}
+
+	// Fetch issues
+	issues := fetchIssues(projectIds)
+	if issues == nil {
+		log.Println("No issues found")
+		return
+	}
+
+	// Categorize issues
+	openIssues, inProgressIssues, closedIssues, doneIssues, todoIssues := categorizeIssues(issues)
+
+	fmt.Print("All Issues: ", openIssues, inProgressIssues, closedIssues, doneIssues, todoIssues, "\n")
+
+	// Create the summary message
+	message := "Daily Overview of Issues:\n\n"
+	message += "*Open Issues:*\n"
+	for _, issue := range openIssues {
+		message += issue + "\n"
+	}
+	message += "\n*In Progress Issues:*\n"
+	for _, issue := range inProgressIssues {
+		message += issue + "\n"
+	}
+	message += "\n*Closed Issues:*\n"
+	for _, issue := range closedIssues {
+		message += issue + "\n"
+	}
+	message += "\n*Done Issues:*\n"
+	for _, issue := range doneIssues {
+		message += issue + "\n"
+	}
+	message += "\n*Todo Issues:*\n"
+	for _, issue := range todoIssues {
+		message += issue + "\n"
+	}
+
+	// Send the message to a Slack channel
+	channelID := os.Getenv("SLACK_OVERVIEW_CHANNEL_ID")
+	_, _, err := slackClient.PostMessage(channelID, slack.MsgOptionText(message, false))
+	if err != nil {
+		log.Fatalf("Error sending message: %v", err)
+	}
 }
