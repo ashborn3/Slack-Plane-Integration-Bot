cce3e6b7b55b750b49b2b73885d1dbe9597d3405
ashborn3 <114130969+ashborn3@users.noreply.github.com>
2024-09-04 10:39:33 +0530

    Refactor fetchProjects function to handle HTTP requests and JSON parsing

--- main.go
+++ main.go
@@ -13,6 +13,17 @@ import (
 )
 
 var slackClient *slack.Client
+var slug string
+var planeToken string
+
+type Project struct {
+	ID string `json:"id"`
+}
+
+type Response struct {
+	Count   int       `json:"count"`
+	Results []Project `json:"results"`
+}
 
 func main() {
 	err := godotenv.Load()
@@ -20,6 +31,8 @@ func main() {
 		log.Fatalf("Error loading .env file!")
 		return
 	}
+	slug = os.Getenv("SLUG")
+	planeToken = os.Getenv("PLANE_TOKEN")
 
 	slackClient = slack.New(os.Getenv("SLACK_TOKEN"))
 
@@ -28,45 +41,49 @@ func main() {
 }
 
 func fetchProjects() []string {
-	var projects []map[string]interface{}
+	var projects Response
 
 	url := fmt.Sprintf("https://api.plane.so/api/v1/workspaces/%s/projects/", os.Getenv("SLUG"))
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Fatalf("Error creating request: %v", err)
-		return
+		return nil
 	}
 
-	req.Header.Add("x-api-key", os.Getenv("PLANE_TOKEN"))
+	req.Header.Add("x-api-key", planeToken)
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		log.Fatalf("Error sending request: %v", err)
-		return
+		return nil
 	}
 
 	defer res.Body.Close()
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
 		log.Fatalf("Error reading response body: %v", err)
-		return
+		return nil
 	}
 
 	err = json.Unmarshal(body, &projects)
 	if err != nil {
 		log.Fatalf("Error unmarshaling response body: %v", err)
-		return
+		return nil
 	}
 
-	projectCount := int(projects["count"])
-	projectResults := []string(projects["results"])
+	projectCount := int(projects.Count)
+	projectResults := projects.Results
 
 	projectId := make([]string, projectCount)
 
 	for i := 0; i < projectCount; i++ {
-		projectId[i] = projectResults[i]["id"]
+		projectId[i] = projectResults[i].ID
 	}
 
 	return projectId
+}
+
+func fetchIssues(projectIds []string) {
+	// you can fetch issues for each project
 }
